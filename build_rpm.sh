1aa895e001806b8c6723b4e42ebb4cef0697f8b0
Major Hayden <major@mhtx.net>
2024-01-26 10:12:35 -0600

    Use pre-built rpm builder container

--- build_rpm.sh
+++ build_rpm.sh
@@ -2,18 +2,13 @@
 set -euxo pipefail
 
 FEDORA_RELEASE=39
-SPEC=/src/golang-github-redhatcloudx-otel-builder.spec
+SPEC=golang-github-redhatcloudx-otel-builder.spec
 
-# Set up packages.
-echo "fastestmirror=1" >>/etc/dnf/dnf.conf
-echo "max_parallel_downloads=20" >>/etc/dnf/dnf.conf
-dnf -qy install git mock rpm-build rpmdevtools
+pushd /src
 
 # Set the right commit SHA in the spec file.
-pushd /src
 CURRENT_COMMIT=$(git rev-parse HEAD)
 sed -i "s/CURRENT_COMMIT_GOES_HERE/${CURRENT_COMMIT}/" $SPEC
-popd
 
 # Build the RPM.
 rpmdev-spectool -R -g $SPEC
